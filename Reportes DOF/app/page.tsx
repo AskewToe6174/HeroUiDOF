04e5ef07b7f7eb8430eeec286fdfc4b7ace7b687
AskewToe6174 <carpinteirosalazari@gmail.com>
2025-08-22 13:26:39 -0600

    Update page.tsx

--- Reportes DOF/app/page.tsx
+++ Reportes DOF/app/page.tsx
@@ -1,6 +1,17 @@
 import { getAcuerdos } from "@/lib/server/services/acuerdos";
 import { getReporteSemanal } from "@/lib/server/services/dof";
+/**
+// Se pueden combinar los filtro
+ // Solo por mes y año
+await getReporteSemanal({ mes: 11, ano: 2024 });
 
+// Solo por cliente
+await getReporteSemanal({ idCliente: 123 });
+
+// Cliente + estación
+await getReporteSemanal({ idCliente: 123, idEstacion: 45 });
+
+ */
 export default async function Home() {
   const acuerdos = await getAcuerdos();
   const semanal = await getReporteSemanal({ mes: 11, ano: 2024, idEstacion: 1 });
