ca4541b5f47a7a21b0b329a512c123556ce46e9e
AskewToe6174 <carpinteirosalazari@gmail.com>
2025-08-22 13:23:50 -0600

    Update page.tsx

--- Reportes DOF/app/page.tsx
+++ Reportes DOF/app/page.tsx
@@ -3,7 +3,7 @@ import { getReporteSemanal } from "@/lib/server/services/dof";
 
 export default async function Home() {
   const acuerdos = await getAcuerdos();
-  const semanal = await getReporteSemanal({ mes: 11, ano: 2024 });
+  const semanal = await getReporteSemanal({ mes: 11, ano: 2024, idEstacion: 1 });
 
   return (
     <pre>{JSON.stringify({  semanal: semanal }, null, 2)}</pre>
